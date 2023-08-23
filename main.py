3a3ce460a3dd4a4215b0172912ac3f4c7640d197
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-08-23 13:13:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Calcula o valor de x usando a fórmula de Bhaskara
+# Versões
+# 0.1 Primeira implementação, para testes
+
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+if a == 0:
+    if b == 0:
+        print("x é indefinido")
+    else:
+        if c == 0:
+            print("x = 0")
+        else:
+            print("x = " + str(c / b * -1))
+else:
+    delta = b ** 2 - 4 * a * c
+    if delta < 0:
+        print("x é indefinido")
+    else:
+        if delta == 0:
+            print("x = " + str((b * -1) / (2 * a)))
+        else:
+            x1 = (b * -1 - delta ** 0.5) / (2 * a)
+            x2 = (b * -1 + delta ** 0.5) / (2 * a)
+            print("x = " + str(x1) + " e " + str(x2))
